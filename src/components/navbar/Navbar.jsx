c04a10416ae4af8b4306c4f5db5a9e5f198b5bf2
naufal.adib.190403@gmail.com <naufal.adib.190403@gmail.com>
2024-09-27 06:34:32 +0700

    update topbar with mui component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,22 +1,72 @@
-import React, { useContext } from "react";
-import { Button } from "@mui/material";
+import React, { useContext, useState } from "react";
 import { auth } from "../../services/firebase";
 import { AuthContext } from "../../services/context/authcontext/AuthContext";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
 
 const Navbar = () => {
-  const { user } = useContext(AuthContext);
+  const { user, isUserLoggedIn } = useContext(AuthContext);
+  const [anchorEl, setAnchorEl] = useState(null);
 
   const logoutButtonHandle = () => {
     auth.signOut();
   };
 
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <div className="py-5 px-5 md:px-12 bg-blue-500 font-semibold text-white flex justify-between items-center">
-      <p>Hi, {user?.displayName || "..."}</p>
-      <Button variant="contained" color="error" onClick={logoutButtonHandle}>
-        Logout
-      </Button>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Hi, {user?.displayName}
+          </Typography>
+          {isUserLoggedIn && (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={logoutButtonHandle}>LogOut</MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
