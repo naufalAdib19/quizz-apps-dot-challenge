6db8e90a35a0b5d279fe6f8f8d1fc85bce81cd52
naufal.adib.190403@gmail.com <naufal.adib.190403@gmail.com>
2024-09-28 18:26:30 +0700

    add image optimizer plugins from vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      jpg: {
+        quality: 100,
+        moazjpeg: true,
+      },
+    }),
+  ],
+});
