2a313b0e8bdf3155febf36e3b1b6595facffbfe5
naufal.adib.190403@gmail.com <naufal.adib.190403@gmail.com>
2024-09-28 18:31:02 +0700

    update path and implement auth context

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,37 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./views/Login.jsx";
+import Home from "./views/Home.jsx";
+import Navbar from "./components/navbar/navbar.jsx";
+import Frame from "./layouts/frame.jsx";
+import { AuthProvider } from "./services/context/authcontext/AuthContext.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Frame />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "/home",
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </AuthProvider>
   </StrictMode>
 );
